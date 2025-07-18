38352ecafdf76238c68579462f81dc62f24e6500
AnkitSingh35 <ankitsingh123p@gmail.com>
2025-07-19 00:55:16 +0530

    Basic Code

---
+++ Sum.java
@@ -0,0 +1,15 @@
+package LocalRepo;
+
+import java.util.Scanner;
+
+public class Sum {
+   
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a : ");
+        int a=sc.nextInt();
+        System.out.print("Enter b : ");
+        int b=sc.nextInt();
+        System.out.println(a+b);
+    }
+}
